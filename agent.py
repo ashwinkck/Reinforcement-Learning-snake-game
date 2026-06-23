590bc44ed9d818d54c33b1479b2baba3dad00b05
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-23 10:22:27 +0530

    train_long_memory_fn

--- agent.py
+++ agent.py
@@ -15,7 +15,8 @@ class Agent:
         self.epsilon = 0 #parameter to control the randomness
         self.gamma = 0 # Discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        
+        self.model = None
+        self.trainer = None
 
 
 
@@ -69,15 +70,23 @@ class Agent:
 
 
     def remember(self, state,action, reward, next_state, done):
-        self.memory.append(state,action, reward, next_state, done) # if it exceeds the max memory popleft if MAX_MEMORY is reached
+        self.memory.append((state, action, reward, next_state, done)) # if it exceeds the max memory popleft if MAX_MEMORY is reached
 
     
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.mini_sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+       
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, next_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
 
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self,state):
         pass
