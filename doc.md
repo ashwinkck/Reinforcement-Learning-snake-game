15059965370da738cde92ef17543c1527c051b95
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-23 13:59:34 +0530

    done_with_the_training_step

--- doc.md
+++ doc.md
@@ -2,3 +2,7 @@
     -the fundamental dilemma of balancing between trying new, unfamiliar actions to gather information (exploration) and leveraging current knowledge to maximize rewards (exploitation) 
 
 2. Feed Forward Neural Net (11 inputs and 3 outputs)
+
+3. torch.squeeze
+
+4. Bellman Equation
--- model.py
+++ model.py
@@ -37,4 +37,37 @@ class QTrainer:
 
     
     def train_step(self, state, action, reward, next_state, done):
-        pass
+        state = torch.tensor(state, dtype=torch.float)    # pytorch tensor format (40-43)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+
+        if len(state.shape) == 1:
+            
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+
+        # 1: predicted Q values with current state
+        pred = = self.model(state)
+
+        target = prd.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+            target[idx][torch.argmax(action).item()] =  Q_new
+
+        #2: Q_new =  r + y * max(next_predicted Q value) -> only do this if it is not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+        self.optimizer.zero_grad()
+        loss = self.criterion(target,pred)
+        loss.backward()
+
+
+        self.optimizer.step()
